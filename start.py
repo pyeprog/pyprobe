df7321fc828ce0e1a474401c23d135ea27acfb1f
Pye <pyeprog@foxmail.com>
2018-10-25 07:59:49 +0800

    simplify project

--- start.py
+++ start.py
@@ -1,43 +1,109 @@
-import atexit
-import time
+import os
 import sys
+from time import time
+from enum import Enum, auto
+from shutil import copytree, rmtree
+from collections import Counter
+from watchdog.observers import Observer  
+from watchdog.events import PatternMatchingEventHandler 
 
-# import click
-from config.types import InjectionType
-from utils.project_utils import get_project_type
+PROBE_RESULT_DIR = os.path.join(__file__, "probe_result")
 
+class ProjectType(Enum):
+    PYTHON = auto()
+    JAVA = auto()
+    TEST = auto()
 
-class Server():
-    def __init__(self, path):
-        self.inject_path = path
-        self.project_type = get_project_type(path)
+postfix_project_type_mapping = {
+        "py"        :   ProjectType.PYTHON,
+        "pyc"       :   ProjectType.PYTHON,
+        "java"      :   ProjectType.JAVA,
+        }
 
-    def start(self):
-        self.inject()
-        atexit.register(self.end)
-        while True:
-            time.sleep(1)
-            print("heart beat")
-
-    def inject(self):
-        if len(self.inject_path) == 0:
-            raise ValueError("Invalid injection path given")
-
-        print("project type is {}".format(self.project_type))
-        print("injection done")
-
-
-    def end(self):
-        print("it's a exit handler")
+project_type_injector_mapping = {
+        ProjectType.PYTHON  :   os.path.join(__file__, "injector", "pyprobe"),
+        ProjectType.JAVA    :   os.path.join(__file__, "injector", "javaprobe"),
+        }
 
 
-# @click.command()
-# @click.option("-p", "--path", "string", default="")
-# def main(path):
-#     server = Server(path)
-#     server.start()
+def list_files_recursively(path):
+    if not os.path.isdir(path):
+        return []
+    res = []
+    for file_path in os.listdir(path):
+        if os.path.isfile(file_path):
+            res.append(file_path)
+        elif os.path.isdir(file_path):
+            cur_dir = os.path.dirname(path)
+            res.extend(list_files_recursively(os.path.join(cur_dir, file_path)))
+
+    return res
+
+def get_project_type(path):
+    file_list = list_files_recursively(path)
+    counter = Counter()
+    for filename in file_list:
+        file_postfix = filename.split(".")[-1]
+        counter[file_postfix] += 1
+
+    most_common_postfix = counter.most_common()[0][0].lower()
+    project_type = postfix_project_type_mapping.get(most_common_postfix, ProjectType.TEST)
+    return project_type
+
+
+class FileHandler(PatternMatchingEventHandler):
+    patterns = ["*.pkl"]
+
+    def process(self, event):
+        """
+        event.event_type 
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        print event.src_path, event.event_type  # print now only for degug
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
 
 if __name__ == '__main__':
-    path = "." if len(sys.argv) < 2 else sys.argv[1]
-    server = Server(path)
-    server.start()
+    if len(sys.argv) <= 1:
+        raise ValueError("injection path is not specified")
+
+    inject_target_path = sys.argv[1]
+    if not os.path.isdir(inject_target_path):
+        raise ValueError("Invalid injection path")
+
+    project_type = get_project_type(inject_target_path)
+    injector_path = project_type_injector_mapping.get(project_type, "")
+
+    if injector_path == "":
+        raise ValueError("No injector matches")
+
+    copytree(injector_path, inject_target_path)
+
+    # begin watching
+    observer = Observer()
+    observer.schedule(FileHandler(), path=PROBE_RESULT_DIR)
+    observer.start()
+    
+    prev_time = time()
+
+    try:
+        while True:
+            time.sleep(1)
+            now_time = time()
+            print("watching for {}s ...".format(now_time - prev_time))
+            prev_time = now_time
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+
